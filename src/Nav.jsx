a253cd143a1710f296bc779faa211a1d08d552e0
IMNOTXS <xs90035190@gmail.com>
2023-01-18 16:53:37 +0300

    finishing the nav section

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import "./Nav.css"
 function Nav() {
   return (
-    <div className='nav'>
-      <h1>this is the nav section</h1>
+    <div className='nav nav_black'>
+      <div className="nav_contents">
+        <img className='nav_logo' src="https://cdn.discordapp.com/attachments/1058878537899180184/1065265114942545920/580b57fcd9996e24bc43c529.png" alt="" />
+        <img className='nav_avatar' src="https://cdn.discordapp.com/attachments/1058878537899180184/1065256700195647568/Netflix-avatar.png" alt="" />
+        
+      </div>
     </div>
   )
 }

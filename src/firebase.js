da5b5aa2623df6c0e71132ee8659bf3ce52ed039
IMNOTXS <xs90035190@gmail.com>
2023-01-18 15:43:40 +0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCnjGVsmzLGVj9bk_nkHmqPQkVLJQs1SCA",
+  authDomain: "netflix-clone-c3217.firebaseapp.com",
+  projectId: "netflix-clone-c3217",
+  storageBucket: "netflix-clone-c3217.appspot.com",
+  messagingSenderId: "911252466568",
+  appId: "1:911252466568:web:6fe9d6ea4e5b9be828a83d"
+};

e0fc7dc6ac567e449878910f07d0002795034091
IMNOTXS <xs90035190@gmail.com>
2023-01-28 09:03:05 +0300

    finished the row components

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -12,14 +12,13 @@ function HomeScreen() {
 
       <Banner />
 
-      <Row title='NETFLIX ORIGINALS'fetchURL={requests.fetchNetflixOriginals} isLargeRow  />
-      <Row title='Top Rated'fetchURL={requests.fetchTopRated} />
-      <Row title='Trending Now'fetchURL={requests.fetchTrending}  />
-      <Row title='Action Movies'fetchURL={requests.fetchActionMovies} />
-      <Row title='Comedy Movies'fetchURL={requests.fetchComedyMovies} />
-      <Row title='Horror Movies'fetchURL={requests.fetchHorrorMovies} />
-      <Row title='Romance Movies'fetchURL={requests.fetchRomanceMovies} />
-      <Row title='Documentaries'fetchURL={requests.fetchDocumentaries} />
+      <Row title='NETFLIX ORIGINALS'fetchUrl={requests.fetchNetflixOriginals} isLargeRow  />
+      <Row title='Top Rated'fetchUrl={requests.fetchTopRated} />
+      <Row title='Action Movies'fetchUrl={requests.fetchActionMovies} />
+      <Row title='Comedy Movies'fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Horror Movies'fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='Romance Movies'fetchUrl={requests.fetchRomanceMovies} />
+      <Row title='Documentaries'fetchUrl={requests.fetchDocumentaries} />
 
 
   

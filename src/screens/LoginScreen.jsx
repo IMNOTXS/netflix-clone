e1f990c7823d9e7ac21d80eb2fa2a71def8d91af
IMNOTXS <xs90035190@gmail.com>
2023-01-30 21:23:13 +0300

    added buttons in the profile section

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useState } from 'react'
 import './LoginScreen.css'
 
 function LoginScreen() {
+
+    const [signIn, setSignIn] = useState(false)
+
   return (
     <div className='loginScreen'>
       <div className="loginScreen_background">
@@ -14,11 +18,21 @@ function LoginScreen() {
         <div className="loginScreen_gradient">
 
         </div>
+
         <div className="loginScreen_body">
           <>
-          <h1>Unlimited films, TV programmes and more.</h1>
+            <h1>Unlimited films, TV programmes and more.</h1>
+            <h2>Watch anywhere. Cancel at any time.</h2>
+            <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+            <div className="loginScreen_input">
+              <form>
+                <input type="email" placeholder='Email Address'  />
+                <button className='loginScreen_getStarted'>GET STARTED</button>
+              </form>
+            </div>
           </>
         </div>
+        
       </div>
     </div>
   )

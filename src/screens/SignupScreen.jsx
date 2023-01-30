4aca23f3b5df5b2e90c12e5e9ee80e5017ca8405
IMNOTXS <xs90035190@gmail.com>
2023-01-30 21:53:28 +0300

    styled the signupscreen class

--- src/screens/SignupScreen.jsx
+++ src/screens/SignupScreen.jsx
@@ -8,6 +8,8 @@ function SignupScreen() {
         <h1>Sign In</h1>
         <input placeholder='Email' type="email" />
         <input placeholder='Password' type="password" />
+        <button type='submit'>Sign In</button>
+        <h4></h4>
       </form>
     </div>
   )
